cd2722f90a79175b25a68660a0e7970b15d3ffd9
NebNooc <49407232+NebNooc@users.noreply.github.com>
2019-12-09 23:06:41 -0500

    Just the spices

--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,22 +8,26 @@ import java.util.Map;
 /**
  * @author leon on 27/12/2018.
  */
-public class Food {
+public class Food <SpiceType extends Class<? extends Spice>> {
 
-    private List<Spice> spices;
-    private Map<Spice, Integer> spiceCount = new HashMap<>();
+    private List<Spice> spices = new ArrayList<>();
+    private Map<SpiceType, Integer> spiceCount = new HashMap<>();
 
     public List<Spice> getAllSpices() {
         return spices;
     }
 
-    public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
-        return null;
+    public Map<SpiceType, Integer> getSpiceCount() {
+        return spiceCount;
     }
 
     public void applySpice(Spice spice) {
         if(!spices.contains(spice))
             spices.add(spice);
 
+//        if(spiceCount.containsKey(spice))
+//            spiceCount.replace(spice, spiceCount.get(spice) + 1);
+//        else
+//            spiceCount.put(spice, 1);
     }
 }
