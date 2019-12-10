f828d3570864d0b7aba359ef5f86efe0c719cd1a
NebNooc <49407232+NebNooc@users.noreply.github.com>
2019-12-09 19:49:29 -0500

    Finished Arrays

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,43 @@
 package rocks.zipcode.quiz4.arrays;
 
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length / 2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        String[] result = new String[values.length - 1];
+        String middleElement = getMiddleElement(values);
+        Integer counter = 0;
+
+        for (String value : values) {
+            if (value.equals(middleElement))
+                continue;
+            result[counter] = value;
+            counter++;
+        }
+        return result;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        String[] result = new String[values.length - 1];
+        String lastElement = getLastElement(values);
+        Integer counter = 0;
+
+        for(int i = 0; i < values.length; i++) {
+            if(i == values.length - 1)
+                continue;
+            result[counter] = values[i];
+            counter++;
+        }
+        return result;
     }
 }
